186e1019baf571a2a2822e86af28412c5a6e8d66
I-am-Gabi <gabicavalcantesilva@gmail.com>
2016-05-22 02:49:05 +0200

    fix readme and finish ring

--- README.md
+++ README.md
@@ -2,3 +2,29 @@
 
 This is the code make for Posix Class, a small set of code to handle with process
 and pipe. 
+
+Run
+---
+
+There is a script ```execute.sh``` in the root directory. You can run it, but probably will be 
+necessary change the permission (making the file readable, writable and executable by everyone). 
+
+    $ chmod 777 execute.sh
+    $ ./execute.sh
+    
+After make this, you can go to the folder ```bin/``` and run the executable. For example:
+    
+    $ ./ring
+    
+
+Protocol
+---------
+
+1. The station 0 will begin asking for the data package (content, destination, etc).
+2. The package will be sending to the destination, and your token will be BUSY
+3. When the package arrived at your destination, it will change the token to ACK 
+4. When the package arrived at your origin station, it will change the token to 
+FREE and send to the next stations.
+5. After, the user will say if he wants send a new package or not.
+    5.a If he answers yes, we go the the step 1.
+    5.b If he doesn't want, the package doesn't change and keep in movement.
--- src/station.c
+++ src/station.c
@@ -8,27 +8,47 @@
 #include <stdlib.h>
 #include "../include/package.h"
 
-
-void interface_station(int station_current) {
+void interface_station(int station_current, struct Package *package_data) {
     int station_to = station_current;
     FILE * input;
     char answer[50];
+    char content[MSG_LEN];
 
     fprintf(stderr, "Do you want send a message? [y/n] ");
     input = fopen("/dev/tty", "a+");
-    fscanf(input, "%s", answer);
-    fclose(input);
+    fscanf(input, "%30[^\n]", answer);
 
     if (!strcmp(answer, "y")) {
         fprintf(stderr, "You are in the station %d. \nWhere you want to send the package? ", station_current);
         input = fopen("/dev/tty", "a+");
         fscanf(input, "%d", &station_to);
-        fclose(input);
+
+        fprintf(stderr, "What is the content package? ");
+        input = fopen("/dev/tty", "a+");
+        fscanf(input, "%30[^\n]", content);
+
+        package_data->tk = BUSY;
+        package_data->transmitters = station_current;
+        package_data->receptors = station_to;
+        strcpy(package_data->content, content);
     }
 
-    // package to transfer from station 0
-    struct Package package_send = { BUSY, station_current, station_to, "package sending..." };
-    write(1, &package_send, PACKAGE_LEN);
+    fclose(input);
+}
+
+void protocol(int station_id, struct Package *package_data) {
+    if (package_data->tk == FREE)
+        interface_station(station_id, package_data);
+
+    // the package find the receptor
+    if (package_data->receptors == station_id) {
+        package_data->tk = ACK;
+        fprintf(stderr, "The package arrived at the destination!\n");
+    }
+    // the package returns to the transmitter
+    else if (package_data->transmitters == station_id && package_data->tk == ACK) {
+        package_data->tk = FREE;
+    }
 }
 
 int main(int argc, char **argv) {
@@ -36,32 +56,29 @@ int main(int argc, char **argv) {
     int station_id = atoi(argv[1]);
 
     // package to receive
-    struct Package package_receive;
+    struct Package package_data;
 
     // check if the station is the station0
-    if (!strcmp(argv[1], "0"))
-        interface_station(station_id);
+    if (!strcmp(argv[1], "0")) {
+        interface_station(station_id, &package_data);
+
+        // write standard output
+        write(1, &package_data, PACKAGE_LEN);
+    }
 
     while (1) {
         // read standard input
-        read(0, &package_receive, PACKAGE_LEN);
-        fprintf(stderr, "station [%s] received : %s from : %d -> to: %d\n",
-                argv[1], package_receive.content, package_receive.transmitters, package_receive.receptors);
+        read(0, &package_data, PACKAGE_LEN);
+        fprintf(stderr, "station [%s] received : [%s] from : %d -> to: %d\n",
+                argv[1], package_data.content, package_data.transmitters, package_data.receptors);
 
-        if (package_receive.tk == FREE)
-            interface_station(station_id);
-
-        // the package find the receptor
-        if (package_receive.receptors == station_id)
-            package_receive.tk = ACK;
-        // the package returns to the transmitter
-        else if (package_receive.transmitters == station_id && package_receive.tk == ACK)
-            package_receive.tk = FREE;
+        // ring protocol
+        protocol(station_id, &package_data);
 
         // sleep 1 second
         sleep(1);
 
         // write standard output
-        write(1, &package_receive, PACKAGE_LEN);
+        write(1, &package_data, PACKAGE_LEN);
     }
 }
